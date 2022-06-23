bce1211e3a9e6b0a6857b2a2309d2ada578f497f
achicyr <hi.cyril@gmail.com>
2022-06-23 13:14:50 +0900

    DONE STEP 8: LES ÉTAPES MANQUANTES DE LA PARTIE PRÉCÉDENTE SONT VALIDÉS: - Les différentes variantes d'un même produit apparaîssent bien sur la liste du panier - le total est affiché (nbre articles et montant total)

--- front/js/cart.js
+++ front/js/cart.js
@@ -3,34 +3,44 @@ const cart = {...localStorage}
 
 const getCartProducts = () => {
     console.log(cart)
+    let totalQty = 0
+    , totalAmount = 0
 
     for(let a in cart) {
         cart[a] = JSON.parse(cart[a])
         let product = cart[a]
-        cart__items.innerHTML += `
-            <article class="cart__item" data-id="${product._id}" data-color="{product-color}">
-                <div class="cart__item__img">
-                  <img src="${product.imageUrl}" alt="${product.altText}">
-                </div>
-                <div class="cart__item__content">
-                  <div class="cart__item__content__description">
-                    <h2>${product.name}</h2>
-                    <p>${product.orders[0].color}</p>
-                    <p>${product.price} €</p>
+        , variants = product.orders
+        totalQty += variants.length
+        totalAmount += product.price * variants.length
+
+        variants.forEach((variant) => {
+          cart__items.innerHTML += `
+              <article class="cart__item" data-id="${product._id}" data-color="${variant.color}">
+                  <div class="cart__item__img">
+                    <img src="${product.imageUrl}" alt="${product.altText}">
                   </div>
-                  <div class="cart__item__content__settings">
-                    <div class="cart__item__content__settings__quantity">
-                      <p>Qté : ${product.orders[0].qty}</p>
-                      <input type="number" class="itemQuantity" name="itemQuantity" min="1" max="100" value="${product.orders[0].qty}">
+                  <div class="cart__item__content">
+                    <div class="cart__item__content__description">
+                      <h2>${product.name}</h2>
+                      <p>${variant.color}</p>
+                      <p>${product.price} €</p>
                     </div>
-                    <div class="cart__item__content__settings__delete">
-                      <p class="deleteItem">Supprimer</p>
+                    <div class="cart__item__content__settings">
+                      <div class="cart__item__content__settings__quantity">
+                        <p>Qté : ${variant.qty}</p>
+                        <input type="number" class="itemQuantity" name="itemQuantity" min="1" max="100" value="${variant.qty}">
+                      </div>
+                      <div class="cart__item__content__settings__delete">
+                        <p class="deleteItem">Supprimer</p>
+                      </div>
                     </div>
                   </div>
-                </div>
-            </article>
-        `
+              </article>
+          `
+        })
     }
+    totalQuantity.innerHTML = totalQty
+    totalPrice.innerHTML = totalAmount
     // console.log(cart);
     
 }
