bdc4c62fc5531d7f3caffa8de0cc0e4875b841a5
achicyr <hi.cyril@gmail.com>
2022-07-14 10:43:20 +0900

    DONE STEP6 Les données du produits sont intégrés sur la page product.html Des commentaires à la fonction et à ses déclarations sont définis, décrivant respectivement son fonctionnement, ses paramètres, et leur objectifs

--- front/js/product.js
+++ front/js/product.js
@@ -7,16 +7,22 @@ console.log(productID);
 getProduct(endpoint, productID)
 
 /**
- * * CHARGE LES DONNÉES DE L'API AUX ENDPOINT DONNÉ EN PARAMÈTRE @param(url)
- * * POUR RECEVOIR LE PRODUIT À AFFICHER SUR CETTE PAGE PRODUIT
+ * * CHARGE LES DONNÉES DE L'API À L'ENDPOINT DONNÉ EN PARAMÈTRE @param(url)+'/'+@param(id)
+ * * POUR RECEVOIR LE PRODUIT À AFFICHER SUR CETTE PAGE PRODUIT 
  * @param {string} url L'uri à requêter, le "endpoint"
  * @param {string} id L'identifiant du produit à afficher
  */
 function getProduct(url = endpoint, id = productID){
     fetch(url+id)
         .then(response => response.json())
-        .then((json) => {
-            console.log(json);
-            
+        .then((product) => {
+            // console.log(product);
+            // JE CHARGE MAINTENANT LE DOM AVEC LES DONNÉE REÇUS DE L'API CONCERNANT LE PRODUIT
+            // IMAGE/TITRE/DESCRIPTION/PRIX/COURLEURS
+            document.querySelector('.item__img').innerHTML = `<img src="${product.imageUrl}" alt="${product.altTxt}">`
+            title.innerHTML = product.name
+            price.innerHTML = product.price
+            description.innerHTML = product.description
+            product.colors.forEach(color=>{colors.innerHTML += `<option value="${color}">${color}</option>`})
         })
 }
